59d57fbf62f9df1482eb764da0d861c1ddb45685
Igrojas <43014174+Igrojas@users.noreply.github.com>
2024-07-08 18:26:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ st.markdown("""
 - 🧮 **Desarrollo de algoritmos para evaluar la influencia y agrupación de autores.**
 - 📊 **Creación de visualizaciones gráficas y grafos para representar la información de manera efectiva.**""")
 
-st.markdown("[Ver proyecto completo](https://ml-flotacion-jdp6tepzersnzkwhyk498a.streamlit.app/)")
+#st.markdown("[Ver proyecto completo](https://ml-flotacion-jdp6tepzersnzkwhyk498a.streamlit.app/)")
