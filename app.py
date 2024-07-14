8acaecdc2bf2ec8bb4f7ff604cfaeb78d0ca2d1b
Igrojas <43014174+Igrojas@users.noreply.github.com>
2024-07-14 13:06:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ st.markdown("""
 st.markdown("""
 [Memoria de título](https://github.com/Igrojas/Memoria-Titulo/blob/main/Memoria%20de%20Titulo%202023.pdf) | 
 [Parte 1](https://an-lisis-bibliom-trico-jmvnh2ttwkckyma3bwpbos.streamlit.app/) | 
-[Parte 2](https://an-lisis-bibliom-trico-scc3chpdbrqxoqnxpyzimx.streamlit.app/)""")
+[Parte 2](https://an-lisis-bibliom-trico-scc3chpdbrqxoqnxpyzimx.streamlit.app/) |
+[Algoritmo AuthorRank](https://an-lisis-bibliom-trico-q347mhuyf9j4wxsl9kdf2c.streamlit.app/)""")
 
 #st.markdown("[Ver proyecto completo](https://ml-flotacion-jdp6tepzersnzkwhyk498a.streamlit.app/)")
