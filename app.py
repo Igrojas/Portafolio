866963597f6a6bb7a91c1ce0e7734028335d153a
Ignacio Rojas <igrojasro@hotmail.com>
2024-08-09 18:59:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.header(":blue[Proyectos Personales]")
 def mostrar_proyecto(titulo, descripcion, imagen_url, link, referencias):
     
     st.write("###")
-    st.write(f"## :green[{titulo}]")
+    st.write(f"### :green[{titulo}]")
     col1, col2 = st.columns([0.7, 0.3])
     with col1:
         
